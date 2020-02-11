24f69199e3cb3aa201d45b2a9ed5b39933c5d575
Sumaid Syed <sumaidsyed@gmail.com>
2020-02-11 11:54:58 +0530

    Set up firebase

--- app/src/main/java/com/example/adhoc/MainActivity.java
+++ app/src/main/java/com/example/adhoc/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.adhoc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mDriver, mCustomer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDriver =  findViewById(R.id.driver);
+        mCustomer =  findViewById(R.id.customer);
+
+        mDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        mCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CustomerLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
 }
