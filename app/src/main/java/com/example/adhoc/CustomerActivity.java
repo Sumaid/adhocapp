677ab62f5f0004cde65deecfb6a3b844f19ff01e
Sumaid Syed <sumaidsyed@gmail.com>
2020-02-16 11:17:05 +0530

    Places Autocomplete setup done on customer side

--- app/src/main/java/com/example/adhoc/CustomerActivity.java
+++ app/src/main/java/com/example/adhoc/CustomerActivity.java
@@ -2,15 +2,33 @@ package com.example.adhoc;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
 
 public class CustomerActivity extends AppCompatActivity {
-    Button mLogout;
+    Button mLogout, mRequest;
+    LatLng destination;
+    String TAG = "CustomerActivity Debug";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +36,8 @@ public class CustomerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_customer);
 
         mLogout = findViewById(R.id.logout);
+        mRequest = findViewById(R.id.request);
+
         mLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +46,50 @@ public class CustomerActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
                 return;
+            }
+        });
+
+        mRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (destination == null){
+                    Toast.makeText(CustomerActivity.this, "Select a destination!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String userid = FirebaseAuth.getInstance().getUid();
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("customerRequest");
+                GeoFire geofire = new GeoFire(ref);
+                geofire.setLocation(userid, new GeoLocation(destination.latitude, destination.longitude), new GeoFire.CompletionListener() {
+                    @Override
+                    public void onComplete(String key, DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+
+        Places.initialize(getApplicationContext(),"AIzaSyC1S7BUHYryWvinNdNtu1s7mn-d1IMgXP0");
+        PlacesClient placesClient = Places.createClient(this);
+
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                destination = place.getLatLng();
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId()+","+place.getLatLng());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
             }
         });
     }
